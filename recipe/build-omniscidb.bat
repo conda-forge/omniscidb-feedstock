1f3ea6cda0aa8131ea4dbd08cac5134c3c53e7a4
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-01-14 23:42:15 -0300

    upd

--- recipe/build-omniscidb.bat
+++ recipe/build-omniscidb.bat
@@ -3,13 +3,27 @@
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
-cmake -G "NMake Makefiles" ^
+COPY %CONDA_PREFIX%\Library\lib\thriftmd.lib %CONDA_PREFIX%\Library\bin\thriftmd.lib
+
+CALL vcpkg install --triplet x64-windows libarchive
+
+cmake -G "Ninja" ^
+      -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake ^
+      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+      -DCMAKE_LIBRARY_PATH=%CONDA_PREFIX%\Library\lib ^
+      -DCMAKE_INCLUDE_PATH=%CONDA_PREFIX%\Library\include ^
+      -DCMAKE_C_COMPILER=cl.exe ^
+      -DCMAKE_CXX_COMPILER=cl.exe ^
       -DENABLE_FOLLY:BOOL=OFF ^
       -DENABLE_TESTS:BOOL=OFF ^
       -DENABLE_CUDA:BOOL=OFF ^
       -DENABLE_GEOS:BOOL=OFF ^
+      -DENABLE_FSI_ODBC:BOOL=OFF ^
       -DENABLE_AWS_S3:BOOL=OFF ^
+      -DENABLE_LDAP:BOOL=OFF ^
+      -DENABLE_CUDA:BOOL=OFF ^
       -DCMAKE_BUILD_TYPE=Release ^
+      -DBoost_NO_BOOST_CMAKE:BOOL=OFF ^
       ..
 if errorlevel 1 exit 1
 
