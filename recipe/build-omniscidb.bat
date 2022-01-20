e703b66d9bb442cfb81024ae60e7ff962c3635e4
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-01-20 14:08:42 -0300

    upd

--- recipe/build-omniscidb.bat
+++ recipe/build-omniscidb.bat
@@ -5,12 +5,15 @@ pushd "%SRC_DIR%"\build
 
 COPY %CONDA_PREFIX%\Library\lib\thriftmd.lib %CONDA_PREFIX%\Library\bin\thriftmd.lib
 
-cmake -G "Ninja" ^
+cmake -G "NMake Makefiles" ^
+      -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
       -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
       -DCMAKE_LIBRARY_PATH=%CONDA_PREFIX%\Library\lib ^
       -DCMAKE_INCLUDE_PATH=%CONDA_PREFIX%\Library\include ^
-      -DCMAKE_C_COMPILER=cl.exe ^
-      -DCMAKE_CXX_COMPILER=cl.exe ^
+      -DLibArchive_INCLUDE_DIR=%CONDA_PREFIX\include ^
+      -DLibArchive_LIBRARY=%CONDA_PREFIX%\Library\lib\archive.lib ^
+      -DLZMA_LIBRARY=%CONDA_PREFIX%\Library\lib\liblzma.lib ^
+      -DBZ2_LIBRARY=%CONDA_PREFIX%\Library\lib\libbz2.lib ^
       -DENABLE_FOLLY:BOOL=OFF ^
       -DENABLE_TESTS:BOOL=OFF ^
       -DENABLE_CUDA:BOOL=OFF ^
@@ -18,7 +21,7 @@ cmake -G "Ninja" ^
       -DENABLE_FSI_ODBC:BOOL=OFF ^
       -DENABLE_AWS_S3:BOOL=OFF ^
       -DENABLE_LDAP:BOOL=OFF ^
-      -DENABLE_CUDA:BOOL=OFF ^
+      -DMAPD_EDITION:STRING=EE ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBoost_NO_BOOST_CMAKE:BOOL=OFF ^
       ..
