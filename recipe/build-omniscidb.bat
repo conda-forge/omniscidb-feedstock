cd7739fdb1879642af68c16f23dbc439a48854a6
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-01-21 01:13:52 -0300

    upd

--- recipe/build-omniscidb.bat
+++ recipe/build-omniscidb.bat
@@ -6,14 +6,10 @@ pushd "%SRC_DIR%"\build
 COPY %PREFIX%\Library\lib\thriftmd.lib %PREFIX%\Library\bin\thriftmd.lib
 
 cmake -G "Ninja" ^
-      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-      -DCMAKE_PREFIX_PATH=%PREFIX% ^
-      -DCMAKE_LIBRARY_PATH=%PREFIX%\Library\lib ^
-      -DCMAKE_INCLUDE_PATH=%PREFIX%\Library\include ^
-      -DLibArchive_INCLUDE_DIR=%PREFIX\include ^
-      -DLibArchive_LIBRARY=%PREFIX%\Library\lib\archive.lib ^
-      -DLZMA_LIBRARY=%PREFIX%\Library\lib\liblzma.lib ^
-      -DBZ2_LIBRARY=%PREFIX%\Library\lib\libbz2.lib ^
+      -DCMAKE_INSTALL_PREFIX=%CONDA_PREFIX% ^
+      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+      -DLZMA_LIBRARY=%CONDA_PREFIX%\Library\lib\liblzma.lib ^
+      -DBZ2_LIBRARY=%CONDA_PREFIX%\Library\lib\libbz2.lib ^
       -DENABLE_FOLLY=OFF ^
       -DENABLE_TESTS=OFF ^
       -DENABLE_CUDA=OFF ^
@@ -23,7 +19,6 @@ cmake -G "Ninja" ^
       -DENABLE_LDAP=OFF ^
       -DMAPD_EDITION=EE ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DBoost_NO_BOOST_CMAKE=OFF ^
       "%SRC_DIR%"
 if errorlevel 1 exit 1
 
